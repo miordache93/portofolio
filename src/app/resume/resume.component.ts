5a4e4cb05f4f50c317ea65995f7b641c38f033af
Mihai Alexandru Iordache <iordache.mihaialexandru@gmail.com>
2020-02-11 21:57:11 +0200

    Fix resume component

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -39,6 +39,125 @@ export class ResumeComponent implements OnInit {
         ` - Ea quis iuvaret expetendis his, te elit voluptua 
           dignissim per, habeo iusto primis ea eam.`
       ]
+    },
+    {
+      id: 2,
+      period: 'FEB 2015 - SEP 2015',
+      role: 'Software Engineer',
+      company: 'TIM & Knight Solutions',
+      details: [
+        ` - Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+        sed diam nonumy eirmod tempor invidunt ut labore et 
+        dolore magna aliquyam erat, sed diam voluptua.`,
+        ` - At vero eos et accusam et justo duo dolores et ea rebum. 
+        Stet clita kasd gubergren, no sea takimata sanctus est 
+        Lorem ipsum dolor sit amet. `,
+        ` - Ea quis iuvaret expetendis his, te elit voluptua 
+          dignissim per, habeo iusto primis ea eam.`
+      ]
+    }
+  ];
+
+  educationEvents = [
+    {
+      id: 0,
+      period: 'OCT 2015 - JULY 2017',
+      role: 'Master of Economic Informatics',
+      location: 'Faculty of Economic Cybernetics, Statistics and Informatics, ASE – Bucharest',
+      details: 
+        ` - Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+        sed diam nonumy eirmod tempor invidunt ut labore et `
+    },
+    {
+      id: 1,
+      period: 'OCT 2012 - JULY 2015',
+      role: 'Bachelor’s Degree in Economic Informatics',
+      location: 'Faculty of Economic Cybernetics, Statistics and Informatics, ASE – Bucharest',
+      details:
+        ` - Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+        sed diam nonumy eirmod tempor invidunt ut labore et 
+        dolore ma `
+    }
+  ];
+
+  codeSkills = [
+    {
+      id: 0,
+      name: 'HTML',
+      level: 100,
+      icon: '<i aria-label="HTML" class="fab fa-html5"></i>'
+    },
+    {
+      id: 1,
+      name: 'CSS',
+      level: 80,
+      icon: '<i class="fab fa-css3-alt"></i>'
+    },
+    {
+      id: 2,
+      name: 'SASS',
+      level: 100,
+      icon: '<i class="fab fa-sass"></i>'
+    },
+    {
+      id: 3,
+      name: 'Docker',
+      level: 80,
+      icon: '<i class="fab fa-docker"></i>'
+    },
+    {
+      id: 4,
+      name: 'Jenkins',
+      level: 40,
+      icon: '<i class="fab fa-jenkins"></i>'
+    },
+    {
+      id: 5,
+      name: 'JavaScript',
+      level: 100,
+      icon: '<i class="fab fa-js-square"></i>'
+    },
+    {
+      id: 6,
+      name: 'Angular',
+      level: 100,
+      icon: '<i class="fab fa-angular"></i>'
+    },
+    {
+      id: 6,
+      name: 'Java',
+      level: 40,
+      icon: '<i class="fab fa-java"></i>'
+    },
+    {
+      id: 7,
+      name: 'Node',
+      level: 90,
+      icon: '<i class="fab fa-node"></i>'
+    },
+    {
+      id: 8,
+      name: 'MongoDb',
+      level: 60,
+      icon: '<i class="fas fa-database"></i>'
+    },
+    {
+      id: 9,
+      name: 'Vue',
+      level: 30,
+      icon: '<i class="fab fa-vuejs"></i>'
+    },
+    {
+      id: 10,
+      name: 'Git',
+      level: 50,
+      icon: '<i class="fab fa-git"></i>'
+    },
+    {
+      id: 11,
+      name: 'Jira',
+      level: 60,
+      icon: '<i class="fab fa-jira"></i>'
     }
   ];
 
